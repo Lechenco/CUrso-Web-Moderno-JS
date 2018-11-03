b3e0c11a5e57f521d8791b12afe788a6cefd3362
Lechenco <glechenco@gmail.com>
2018-11-03 17:16:14 -0300

    Controle archives

---
+++ Controle/if.js
@@ -0,0 +1,16 @@
+function seForVerdadeEuFalo(valor){
+    if(valor)   
+        console.log('É verdeade...' + valor)
+}
+
+seForVerdadeEuFalo()
+seForVerdadeEuFalo(null)
+seForVerdadeEuFalo(undefined)
+seForVerdadeEuFalo(NaN)
+seForVerdadeEuFalo('')
+seForVerdadeEuFalo(0)
+seForVerdadeEuFalo(-1)
+seForVerdadeEuFalo(' ')
+seForVerdadeEuFalo('?')
+seForVerdadeEuFalo([])
+seForVerdadeEuFalo({})
