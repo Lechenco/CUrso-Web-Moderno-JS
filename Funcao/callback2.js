969573eab9d1110664da7165c4c8ffc2cb42f9dc
Lechenco <glechenco@gmail.com>
2018-11-12 13:59:22 -0200

    Callback Fucntion

---
+++ Funcao/callback2.js
@@ -0,0 +1,19 @@
+const notas = [7.7, 6.5, 5.2, 8.9, 7.1, 9.8]
+
+// Sem Callback
+let notasBaixas = []
+for(let i in notas) {
+    if (notas[i] < 7) {
+        notasBaixas.push(notas[i])
+    }
+}
+
+console.log(notasBaixas)
+
+// Com callback
+
+notasBaixas = notas.filter(function(nota) {
+    return nota < 7
+})
+
+console.log(notasBaixas)
