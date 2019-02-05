da4026a7e7c9bae6be33559936337c41b8d59aef
lechenco-aspire <glechenco@gmail.com>
2019-02-05 13:13:23 -0200

    Bootstrap Project

---
+++ bootstrap/projeto-galeria/src/index.js
@@ -0,0 +1,12 @@
+// Arquivo Sass
+import './scss/index.scss'
+
+// Dependencias
+import 'jquery'
+import 'bootstrap'
+
+// Meus arquivos
+import './js/core/includes'
+
+// Plugin
+import './js/plugins/cityButtons'
---
+++ bootstrap/projeto-galeria/src/js/core/includes.js
@@ -0,0 +1,29 @@
+import $ from 'jquery'
+
+const loadHtmlSuccessCallback = []
+
+export function onLoadHtmlSuccess(callback) {
+    if(!loadHtmlSuccessCallback.includes(callback)){
+        loadHtmlSuccessCallback.push(callback)
+    }
+}
+
+function loadIncludes(parent) {
+    if(!parent) parent = 'body'
+    $(parent).find('[wm-include]').each(function(i, e) {
+        const url = $(e).attr('wm-include')
+        $.ajax({
+            url,
+            success(data) {
+                $(e).html(data)
+                $(e).removeAttr('wm-include')
+
+                loadHtmlSuccessCallback.forEach(
+                    callback => callback(data))
+                loadIncludes(e)
+            }
+        })
+    })
+}
+
+loadIncludes()
