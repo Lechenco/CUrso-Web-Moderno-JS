0575176d4d63a2c4fe232a58bbeff19f9515a8e0
lechenco-aspire <glechenco@gmail.com>
2019-02-06 20:19:23 -0200

    React Calculator Project

--- react/exercicios/src/componentes/Saudacao.jsx
+++ react/exercicios/src/componentes/Saudacao.jsx
@@ -6,13 +6,18 @@ export default class Saudacao extends Component {
     //     nome: 'Fábio'
     // }
 
-    state = this.props
+    state = {...this.props}
+
+    constructor(props) {
+        super(props)
+        this.setTipo = this.setTipo.bind(this)
+    }
 
     setTipo(e) {
         this.setState({ tipo: e.target.value })
     } 
 
-    setNome(e) {
+    setNome = (e) => {
         this.setState({ nome: e.target.value })
     }
 
@@ -23,9 +28,9 @@ export default class Saudacao extends Component {
                 <h1>{tipo} {nome}!</h1>
                 <hr/>
                 <input type="text" placeholder="Tipo.." 
-                    value={tipo} onChange={e => this.setTipo(e)}/>
+                    value={tipo} onChange={this.setTipo}/>
                 <input type="text" placeholder="Nome.." 
-                    value={nome} onChange={e => this.setNome(e)}/>
+                    value={nome} onChange={ this.setNome}/>
             </div>
         ) 
     }
--- react/exercicios/src/index.js
+++ react/exercicios/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from 'react-dom'
 
 // import Primeiro from './componentes/primeiro'
 // import BomDia from './componentes/BomDIa'
-import Saudacao from './componentes/Saudacao'
+// import Saudacao from './componentes/Saudacao'
+import Pai from './componentes/Pai'
+import Filho from './componentes/Filho'
 
-ReactDOM.render(<Saudacao nome = 'Gabriel' tipo='Bom dia'/>, document.getElementById('root'))
+ReactDOM.render(
+    <div>
+        <Pai nome = 'Gabriel' sobrenome='Lechenco'>
+            <Filho nome="Lucas" sobrenome='Lechenco'/>
+        </Pai> 
+
+    </div>,    
+    document.getElementById('root'))
